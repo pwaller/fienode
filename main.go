b7361bdbcd91d267e3cfe3be39a0b3a4a2d9eedb
Peter Waller <p@pwaller.net>
2016-04-17 08:49:58 +0100

    First implementation

--- main.go
+++ main.go
@@ -1,8 +1,76 @@
 package main
 
-import "log"
+import (
+	"crypto"
+	_ "crypto/sha1"
+	"encoding/hex"
+	"fmt"
+	"log"
+	"os"
+
+	"github.com/frostschutz/go-fibmap"
+)
+
+func getExtents(filename string) ([]fibmap.Extent, error) {
+	fd, err := os.Open(filename)
+	if err != nil {
+		return nil, err
+	}
+	defer fd.Close()
+
+	fm := fibmap.NewFibmapFile(fd)
+
+	bsz, errno := fm.Figetbsz()
+	if errno != 0 {
+		return nil, fmt.Errorf("figetbsz: %v", errno)
+	}
+
+	stat, err := fd.Stat()
+	if err != nil {
+		return nil, fmt.Errorf("fstat: %v", err)
+	}
+	size := stat.Size()
+
+	blocks := uint32((size-1)/int64(bsz)) + 1
+
+	extents, errno := fm.Fiemap(blocks)
+	if errno != 0 {
+		return nil, fmt.Errorf("fiemap: %v", errno)
+	}
+	return extents, nil
+}
+
+func getFIENode(filename string) (string, error) {
+	extents, err := getExtents(filename)
+	if err != nil {
+		return "", err
+	}
+
+	w := crypto.SHA1.New()
+	for _, e := range extents {
+		fmt.Fprintf(w, "%d + %d\n", e.Physical>>12, e.Length>>12)
+	}
+
+	return hex.EncodeToString(w.Sum(nil)), nil
+}
 
 func main() {
 	log.SetFlags(0)
-	log.Fatal("Unimplemented")
+	log.SetPrefix("fienode: ")
+
+	if len(os.Args) < 2 {
+		fmt.Fprintln(os.Stderr, "usage: fienode <filename>...")
+		os.Exit(1)
+	}
+
+	for _, filename := range os.Args[1:] {
+		fienode, err := getFIENode(filename)
+		if err != nil {
+			log.Fatal(err)
+		}
+		_, err = fmt.Println(filename, fienode)
+		if err != nil {
+			log.Fatal(err)
+		}
+	}
 }
